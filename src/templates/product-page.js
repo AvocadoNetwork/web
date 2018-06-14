41cc0a5490229f215e3f5c3a1d2f480a3aaae8da
peter-evans <peter-evans@users.noreply.github.com>
2018-06-14 14:46:32 +0900

    Modified layout

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -25,19 +25,12 @@ export const ProductPageTemplate = ({
                 className="full-width-image-container margin-top-0"
                 style={{ backgroundImage: `url(${image})` }}
               >
-                <h2
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                  }}
-                >
+                <div className="container">
                   <img
-                        style={{ width: '500px' }}
-                        src={title}
-                      />
-                </h2>
+                    style={{ width: '500px' }}
+                    src={title}
+                  />
+                </div>
               </div>
               <div className="columns">
                 <div className="column is-7">

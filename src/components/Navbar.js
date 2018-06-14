eb5e5cadbcb16bac8f709740a9c2b764c1defb79
peter-evans <peter-evans@users.noreply.github.com>
2018-06-14 12:23:09 +0900

    Added navbar links to social media

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import twitter from '../img/Twitter_Logo_Blue.svg'
+import facebock from '../img/Facebook_icon.svg'
+import telegram from '../img/Telegram_logo.svg'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
@@ -23,6 +26,36 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-end">
+        <a
+          className="navbar-item"
+          href="https://twitter.com/AvocadoToken"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={twitter} alt="Twitter" />
+          </span>
+        </a>
+        <a
+          className="navbar-item"
+          href="https://www.facebook.com/groups/808615902673092/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={facebook} alt="Facebook" />
+          </span>
+        </a>
+        <a
+          className="navbar-item"
+          href="https://t.me/AVOcadoToken"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={telegram} alt="Telegram" />
+          </span>
+        </a>        
         <a
           className="navbar-item"
           href="https://github.com/AvocadoNetwork"
